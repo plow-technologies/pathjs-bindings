e884eaa861547f9691c9c95724255899455aa13c
Michael <michael.sewell@plowtech.net>
2015-10-09 18:29:24 -0500

    added rootPath function for setting root path

--- src/PathJS/RouteHandler.hs
+++ src/PathJS/RouteHandler.hs
@@ -13,10 +13,15 @@ import           GHCJS.Types
 foreign import javascript unsafe "Path.map($1).to(function() { var newRoute = $2; newRoute(); });" jq_Path  :: JSString -> Callback (IO ())  -> IO JSRef
 foreign import javascript unsafe "Path.listen();" jq_Path_Listen  ::  IO ()
 
+foreign import javascript unsafe "Path.root($1);" jq_Path_Root :: JSString -> IO ()
+
 createPath :: JSString -> Callback (IO ()) -> IO JSRef 
 createPath = jq_Path
 
 listenPath :: IO ()
 listenPath = jq_Path_Listen
+
+rootPath :: JSString -> IO ()
+rootPath = jq_Path_Root
 
 
